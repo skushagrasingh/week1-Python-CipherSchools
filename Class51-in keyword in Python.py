d219e855e90f5acfdc27d3264885ee4e1b3aad8f
skushagrasingh <119047180+skushagrasingh@users.noreply.github.com>
2022-12-05 00:29:17 +0530

    Add files via upload

---
+++ Class51-in keyword in Python.py
@@ -0,0 +1,9 @@
+name = "Harshit"
+# in key Harshit: str
+# if with in
+
+if 'H' in "Harshit":
+    print("H is present in name")
+
+else:
+    print("not present")
---
+++ Class54-Sum of Numbers Program using while loop in Python.py
@@ -0,0 +1,7 @@
+total = 0
+i = 1 # i = 2
+while i <= 10:
+    total += i
+    i += 1
+print(total)
+# total = 0+ 1+2+3+4+
