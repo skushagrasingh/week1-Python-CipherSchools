187679b8ec1c768527bb5e9e59056c35873aead1
skushagrasingh <119047180+skushagrasingh@users.noreply.github.com>
2022-12-04 23:39:29 +0530

    Add files via upload

---
+++ Class20-String_Concatenation_in_Python.py
@@ -0,0 +1,14 @@
+first_name = "Harshit"
+
+last_name = "Vashistha"
+
+full_name = first_name +" " + last_name
+
+# print(full_name)
+
+# print(first_name + 3) error
+
+# print(first_name + "3") no error
+
+# print(first_name + str(3)) # no error I
+print(first_name * 3)
